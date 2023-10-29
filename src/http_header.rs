8267637682b61dd76dddce3555d4b99ababd4eab
Sleeyax <yourd3veloper@gmail.com>
2023-10-29 23:57:36 +0100

    feat: add HTTP header

---
+++ src/http_header.rs
@@ -0,0 +1,76 @@
+use std::ffi::{CStr, CString};
+
+use crate::{
+    Cronet_HttpHeaderPtr, Cronet_HttpHeader_Create, Cronet_HttpHeader_Destroy,
+    Cronet_HttpHeader_name_get, Cronet_HttpHeader_name_set, Cronet_HttpHeader_value_get,
+    Cronet_HttpHeader_value_set, Destroy,
+};
+
+pub struct HttpHeader {
+    pub(crate) ptr: Cronet_HttpHeaderPtr,
+}
+
+impl HttpHeader {
+    pub fn new() -> Self {
+        unsafe {
+            HttpHeader {
+                ptr: Cronet_HttpHeader_Create(),
+            }
+        }
+    }
+
+    /// Get the name of this header.
+    pub fn name(&self) -> String {
+        unsafe {
+            let c_name = Cronet_HttpHeader_name_get(self.ptr);
+            let name = std::ffi::CStr::from_ptr(c_name)
+                .to_string_lossy()
+                .into_owned();
+            name
+        }
+    }
+
+    /// Set the name of this header.
+    pub fn set_name(&self, name: &str) {
+        unsafe {
+            let c_name = std::ffi::CString::new(name).unwrap();
+            Cronet_HttpHeader_name_set(self.ptr, c_name.as_ptr());
+        }
+    }
+
+    pub fn value(&self) -> String {
+        unsafe {
+            let c_value = Cronet_HttpHeader_value_get(self.ptr);
+            let value = CStr::from_ptr(c_value).to_string_lossy().into_owned();
+            value
+        }
+    }
+
+    pub fn set_value(&self, value: &str) {
+        unsafe {
+            let c_value = CString::new(value).unwrap();
+            Cronet_HttpHeader_value_set(self.ptr, c_value.as_ptr());
+        }
+    }
+}
+
+impl Destroy for HttpHeader {
+    fn destroy(&self) {
+        unsafe { Cronet_HttpHeader_Destroy(self.ptr) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Destroy;
+
+    #[test]
+    fn test_http_header() {
+        let http_header = super::HttpHeader::new();
+        http_header.set_name("name");
+        http_header.set_value("value");
+        assert_eq!(http_header.name(), "name");
+        assert_eq!(http_header.value(), "value");
+        http_header.destroy();
+    }
+}
