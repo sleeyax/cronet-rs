567601a373bfbd51a539eefe8d82a24ef140c860
Sleeyax <yourd3veloper@gmail.com>
2023-10-30 16:51:05 +0100

    feat: add executor & runnable

---
+++ src/executor.rs
@@ -0,0 +1,68 @@
+use once_cell::sync::Lazy;
+
+use crate::{
+    state::CronetCallbacks, Cronet_ExecutorPtr, Cronet_Executor_CreateWith,
+    Cronet_Executor_Destroy, Cronet_Executor_Execute, Cronet_RunnablePtr, Destroy, Runnable,
+};
+
+static mut EXECUTOR_CALLBACKS: Lazy<CronetCallbacks<Cronet_ExecutorPtr, ExecutorExecuteFn>> =
+    Lazy::new(|| CronetCallbacks::new());
+
+#[no_mangle]
+unsafe extern "C" fn cronetExecutorOnExecute(
+    selfPtr: Cronet_ExecutorPtr,
+    runnablePtr: Cronet_RunnablePtr,
+) {
+    let mut lockedMap = EXECUTOR_CALLBACKS.map().lock().unwrap();
+    lockedMap.remove(&selfPtr);
+    if let Some(callback) = lockedMap.get(&selfPtr) {
+        callback(Executor { ptr: selfPtr }, Runnable { ptr: runnablePtr });
+    }
+}
+
+pub type ExecutorExecuteFn = fn(executor: Executor, runnable: Runnable);
+
+/// An interface provided by the app to run a [Runnable] asynchronously.
+pub struct Executor {
+    pub(crate) ptr: Cronet_ExecutorPtr,
+}
+
+impl Executor {
+    pub fn new(on_execute: ExecutorExecuteFn) -> Self {
+        unsafe {
+            let ptr = Cronet_Executor_CreateWith(Some(cronetExecutorOnExecute));
+            EXECUTOR_CALLBACKS
+                .map()
+                .lock()
+                .unwrap()
+                .insert(ptr, on_execute);
+            Self { ptr }
+        }
+    }
+
+    pub fn execute(&self, runnable: Runnable) {
+        unsafe {
+            Cronet_Executor_Execute(self.ptr, runnable.ptr);
+        }
+    }
+}
+
+impl Destroy for Executor {
+    fn destroy(&self) {
+        unsafe { Cronet_Executor_Destroy(self.ptr) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Destroy;
+
+    #[test]
+    fn test_executor() {
+        // TODO: test if the executor actually runs the runnable
+        let executor = super::Executor::new(|_, _| {});
+        let runnable = super::Runnable::new(|_| {});
+        executor.execute(runnable);
+        executor.destroy();
+    }
+}
---
+++ src/runnable.rs
@@ -0,0 +1,66 @@
+use once_cell::sync::Lazy;
+
+use crate::{
+    state::CronetCallbacks, Cronet_RunnablePtr, Cronet_Runnable_CreateWith,
+    Cronet_Runnable_Destroy, Cronet_Runnable_Run, Destroy,
+};
+
+static mut RUNNABLE_CALLBACKS: Lazy<CronetCallbacks<Cronet_RunnablePtr, RunnableRunFn>> =
+    Lazy::new(|| CronetCallbacks::new());
+
+#[no_mangle]
+unsafe extern "C" fn cronetRunnableOnRun(selfPtr: Cronet_RunnablePtr) {
+    let mut lockedMap = RUNNABLE_CALLBACKS.map().lock().unwrap();
+    lockedMap.remove(&selfPtr);
+    if let Some(callback) = lockedMap.get(&selfPtr) {
+        callback(Runnable { ptr: selfPtr });
+    }
+}
+
+pub type RunnableRunFn = fn(runnable: Runnable);
+
+/// An interface to run commands on the Executor.
+///
+/// Note: In general, creating Runnables should only be done by Cronet.
+/// Runnables created by the app don't have the ability to perform operations when the [Runnable] is being destroyed (i.e. by `Cronet_Runnable_Destroy``)
+/// so resource leaks are possible if the [Runnable] is posted to an [Executor] that is being shutdown with unexecuted Runnables.
+/// In controlled testing environments deallocation of associated resources can be performed in `run()` if the runnable can be assumed to always be executed.
+pub struct Runnable {
+    pub(crate) ptr: Cronet_RunnablePtr,
+}
+
+impl Runnable {
+    pub fn new(on_run: RunnableRunFn) -> Self {
+        unsafe {
+            let ptr: *mut crate::Cronet_Runnable =
+                Cronet_Runnable_CreateWith(Some(cronetRunnableOnRun));
+            RUNNABLE_CALLBACKS.map().lock().unwrap().insert(ptr, on_run);
+            Self { ptr }
+        }
+    }
+
+    pub fn run(&self) {
+        unsafe {
+            Cronet_Runnable_Run(self.ptr);
+        }
+    }
+}
+
+impl Destroy for Runnable {
+    fn destroy(&self) {
+        unsafe { Cronet_Runnable_Destroy(self.ptr) }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Destroy;
+
+    #[test]
+    fn test_runnable() {
+        let runnable = super::Runnable::new(|_| {});
+        // TODO: test if the runnable actually executes the callback
+        runnable.run();
+        runnable.destroy();
+    }
+}
