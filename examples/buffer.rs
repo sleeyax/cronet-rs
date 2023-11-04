691e738a098e911a2047b1b638a4e2cc29dfb8ff
Sleeyax <yourd3veloper@gmail.com>
2023-11-04 21:10:49 +0100

    refactor: remove useless example
    See Buffer's tests instead.

--- examples/buffer.rs
+++
@@ -1,8 +0,0 @@
-use cronet_rs::{Buffer, Destroy};
-
-fn main() {
-    let buffer = Buffer::new();
-    buffer.init_size(10);
-    println!("Buffer size: {}", buffer.size());
-    buffer.destroy();
-}
